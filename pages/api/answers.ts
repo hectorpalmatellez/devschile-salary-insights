22a9ad2497212f25c50ddcb95afe3d83f5dfffee
Héctor Palma Téllez <hector@palmatellez.cl>
2022-08-29 22:37:19 -0400

    feat(v1): remove axios dependency and use fetch

--- pages/api/answers.ts
+++ pages/api/answers.ts
@@ -1,15 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
-import { Answer } from '../../types/Answer';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    await axios.get(process.env.ANSWERS_URL as string).then((response) => {
-      res.status(200).json(response?.data as Array<Answer>);
-    });
+    const response = await fetch(process.env.ANSWERS_URL as string);
+    res.status(200).json(await response.json());
   } catch (error) {
     console.error(error);
   }
